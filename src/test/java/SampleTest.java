72fae61f00ff94a27f0a25baf90fb3d4133109d6
l0c4lh0s7 <sumit.bhakt@mavenwave.com>
2018-12-04 18:38:08 +0530

    some tests

--- src/test/java/SampleTest.java
+++ src/test/java/SampleTest.java
@@ -5,9 +5,16 @@ import org.junit.Test;
 public class SampleTest {
 
 	@Test
-	public void simpleTest() {
+	public void simpleTestPass() {
 		int a = 1;
 		int b = 2;
 		assertTrue( a + b == 3 );
 	}
+	
+//	@Test
+//	public void simpleTestFail() {
+//		int a = 1;
+//		int b = 2;
+//		assertTrue( a + b == 4 );
+//	}
 }
